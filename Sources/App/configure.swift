4404753cfcdfe450ca330168449a96820daf6d4b
Liefran Satrio Sim <liefran123@gmail.com>
2024-06-10 11:20:31 +0700

    add file

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,9 +1,38 @@
 import Vapor
+import APNS
+import APNSCore
+import VaporAPNS
+import JWT
+import Crypto
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
-    try routes(app)
+    let privateKeyURL = URL(fileURLWithPath: "/Users/liefransatriosim/Desktop/notificationServer/Sources/App/AuthKey S95F7MWX5R.p8")
+    let privateKeyData = try Data(contentsOf: privateKeyURL)
+    let privateKeyString = String(data: privateKeyData, encoding: .utf8)!
+    let privateKey = try P256.Signing.PrivateKey(pemRepresentation: privateKeyString)
+
+    // Configure APNS client with JWT authentication
+    let apnsConfig = APNSClientConfiguration(
+        authenticationMethod: .jwt(
+            privateKey: privateKey,
+            keyIdentifier: "S95F7MWX5R",
+            teamIdentifier: "DJFUU6576B"
+        ),
+        environment: .sandbox
+    )
+    
+    app.apns.containers.use(
+        apnsConfig,
+        eventLoopGroupProvider: .shared(app.eventLoopGroup),
+        responseDecoder: JSONDecoder(),
+        requestEncoder: JSONEncoder(),
+        as: .default
+    )
+    
+        // Routes
+        try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,6 @@
 import Vapor
+import APNS
+import APNSCore
 
 func routes(_ app: Application) throws {
     app.get { req async in
@@ -7,5 +9,41 @@ func routes(_ app: Application) throws {
 
     app.get("hello") { req async -> String in
         "Hello, world!"
+    }
+
+    app.post("send-notification") { req async throws -> HTTPStatus in
+        struct NotificationRequest: Content {
+            var token: String
+            var title: String
+            var body: String
+        }
+        
+        struct Payload: Codable {
+            let acme1: String
+            let acme2: Int
+        }
+    
+        let notificationRequest = try req.content.decode(NotificationRequest.self)
+        
+        let payload = Payload(acme1: "hey", acme2: 2)
+            
+            let alert = APNSAlertNotification(
+                alert: .init(
+                    title: .raw(notificationRequest.title),
+                    subtitle: .raw(notificationRequest.body)
+                ),
+                expiration: .immediately,
+                priority: .immediately,
+                topic: "com.liefransim.couple-telecathic",
+                payload: payload
+            )
+
+            // Send the notification
+            try await req.apns.client.sendAlertNotification(
+                alert,
+                deviceToken: notificationRequest.token
+            )
+        
+        return .ok
     }
 }
