2373c435467167f3e5b80618b304c0a141b4ce02
ssabrut <michael.gunawan2002@gmail.com>
2024-06-10 18:57:59 +0700

    Change key to Public directory

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,15 +2,15 @@ import Vapor
 import APNS
 import APNSCore
 import VaporAPNS
-import JWT
+import JWTc
 import Crypto
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
-    let privateKeyURL = URL(fileURLWithPath: "/Users/liefransatriosim/Desktop/notificationServer/Sources/App/AuthKey S95F7MWX5R.p8")
+    let privateKeyURL = URL(fileURLWithPath: app.directory.publicDirectory + "AuthKey S95F7MWX5R.p8")
     let privateKeyData = try Data(contentsOf: privateKeyURL)
     let privateKeyString = String(data: privateKeyData, encoding: .utf8)!
     let privateKey = try P256.Signing.PrivateKey(pemRepresentation: privateKeyString)
